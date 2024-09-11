6d6394728390cf6f41eb22c63adc8f93bb43a460
Kyle Rankin <krankin@gmail.com>
2024-09-11 10:45:58 -0500

    H4X0R News, InspoQuotes, Business card app added
    H4X0R News application, InspoQuotes application with in-app purchasing, and business card added

---
+++ H4X0R News/H4X0R News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Kyle Rankin on 8/22/24.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
---
+++ KyleRankinCard/KyleRankinCard/KyleRankinCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KyleRankinCardApp.swift
+//  KyleRankinCard
+//
+//  Created by Kyle Rankin on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct KyleRankinCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
