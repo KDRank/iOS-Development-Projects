01aae717400820628783e48bc98ca10d090ac4e6
Kyle Rankin <krankin@gmail.com>
2024-09-11 10:39:05 -0500

    ByteCoin and ARDicee added
    crypto application ByteCoin added, Dicee game with AR integration added

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Kyle Rankin on 8/17/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
